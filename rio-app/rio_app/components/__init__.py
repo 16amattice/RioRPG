3343e4b2c55047789e024b685189e4de03db6d5f
Anthony Mattice <anthonymattice@gmail.com>
2024-05-21 12:39:14 -0500

    initial commit

---
+++ rio-app/rio_app/components/__init__.py
@@ -0,0 +1,4 @@
+from .footer import Footer
+from .navbar import Navbar
+from .news_article import NewsArticle
+from .testimonial import Testimonial
---
+++ rio-app/rio_app/pages/root_page.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from dataclasses import KW_ONLY, field
+from typing import *
+
+import rio
+
+from .. import components as comps
+
+class RootPage(rio.Component):
+    def build(self) -> rio.Component:
+        return rio.Row(
+            comps.Navbar(),
+            rio.PageView(),
+        )
